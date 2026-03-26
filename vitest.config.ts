4a02ca0cc18f109c64a7f1915ced73b65cbd1793
ogabrielluiz <gabrielf.almeida90@gmail.com>
2026-03-26 07:32:37 -0300

    Increase test timeout for CI (STL tests need more time)

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,5 +14,6 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./src/test-setup.ts'],
     exclude: ['e2e/**', 'node_modules/**', 'worker/**'],
+    testTimeout: 30000,
   },
 })
